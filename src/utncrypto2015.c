cf63a8619f097aa965726a4e3df55c3c6506697e
Federico <federicotula@gmail.com>
2015-11-07 14:23:34 -0300

    Hay que revisarlo. Hay un metodo test que simplifica y parece funcionar

--- src/utncrypto2015.c
+++ src/utncrypto2015.c
@@ -22,44 +22,83 @@ void inicia_ctx(ECRYPT_ctx * ctx);
 void cifrador(int accion, char * path_origen, char * path_destino);
 
 int main(int argc, char **argv) {
+	/*
+	 int accion = 0;
 
-	int accion = 0;
+	 if (argv[1][0] == 48){
+	 accion = 0;
+	 } else {
+	 accion = 1;
+	 }
 
-	if (argv[1][0] == 48){
-		accion = 0;
-	} else {
-		accion = 1;
-	}
+	 cifrador(accion, argv[2], argv[3]);
+	 */
 
-	cifrador(accion, argv[2], argv[3]);
-
-/*
-	char* imagen = "C:\\Users\\admin\\git\\utncrypto2015\\src\\imagen.bmp";
-	char* imagen_encrypt =
-			"C:\\Users\\admin\\git\\utncrypto2015\\src\\imagen_encrypt.bmp";
-	char* imagen_decrypt =
-			"C:\\Users\\admin\\git\\utncrypto2015\\src\\imagen_decrypt.bmp";
-
+	char* imagen = "/home/utnso/git/utncrypto2015/src/imagen.bmp";
+	char* imagen_encrypt = "/home/utnso/git/utncrypto2015/src/imagen_e.bmp";
+	char* imagen_decrypt = "/home/utnso/git/utncrypto2015/src/imagen_d.bmp";
 
 	// CIFRO
 	cifrador(CIFRAR, imagen, imagen_encrypt);
 
 	// DECIFRO
 	cifrador(DECIFRAR, imagen_encrypt, imagen_decrypt);
-*/
+
+	test();
+
 	return EXIT_SUCCESS;
+}
+
+void test() {
+	int tamanio = 53 * sizeof(char);
+	int tamanio_lectura = tamanio_archivo(
+			"/home/utnso/git/utncrypto2015/src/imagen.bmp") - tamanio;
+	int tamanio_escritura = tamanio_lectura * 3;
+	FILE *archivo = fopen("/home/utnso/git/utncrypto2015/src/imagen.bmp", "rb");
+	FILE *salida = fopen("/home/utnso/git/utncrypto2015/src/imagen2.bmp", "w");
+	FILE *salida_medio = fopen("/home/utnso/git/utncrypto2015/src/imagen12.bmp", "w");
+
+	u8 iv = 0;
+	ECRYPT_ctx * ctx = malloc(sizeof(ECRYPT_ctx));
+	inicia_ctx(ctx);
+	puts("Encripto 'A'");
+	char * encabezado = malloc(tamanio);
+	fgets(encabezado, tamanio, archivo);
+
+	char * lector = malloc(tamanio_lectura);
+	char * output = malloc(tamanio_escritura);
+	char * original = malloc(tamanio_lectura);
+	char * impresion = malloc(tamanio_lectura + tamanio);
+	char * intermedio = malloc(tamanio_escritura + tamanio);
+	fread(lector, tamanio_lectura, 1, archivo);
+	ECRYPT_process_packet(0, ctx, &iv, lector, output, tamanio_lectura);
+
+	inicia_ctx(ctx);
+	iv = 0;
+
+	ECRYPT_process_packet(1, ctx, &iv, output, original, tamanio_lectura);
+	memcpy(impresion, encabezado, tamanio);
+	memcpy(impresion + tamanio, original, tamanio_lectura);
+	fwrite(impresion, tamanio_lectura + tamanio, 1, salida);
+
+	memcpy(intermedio, encabezado, tamanio);
+	memcpy(intermedio + tamanio, output, tamanio_escritura);
+	fwrite(intermedio, tamanio_escritura + tamanio, 1, salida_medio);
+
 }
 
 long int tamanio_archivo(char* path) {
 
 	FILE * fichero;
-	double long tamanio = 0;
+	long int tamanio = 0;
 
 	fichero = fopen(path, "r");
-	fseek(fichero, 0L, SEEK_END);
 
-	tamanio = ftell(fichero);
-
+	while (!feof(fichero)) {
+		fgetc(fichero);
+		tamanio++;
+	}
+	tamanio--;
 	fclose(fichero);
 
 	return tamanio;
@@ -86,9 +125,11 @@ void cifrador(int accion, char * path_origen, char * path_destino) {
 	u8 iv = 0;
 	ECRYPT_ctx * ctx = malloc(sizeof(ECRYPT_ctx));
 	inicia_ctx(ctx);
+	long int tamanio_escritura, tamanio_lectura;
 
 	int tamanio = 53 * sizeof(char);
-	char encabezado[tamanio];
+	char * encabezado = malloc(tamanio);
+	tamanio_lectura = tamanio_archivo(path_origen) - tamanio;
 
 	archivo_origen = fopen(path_origen, "rb");
 	archivo_destino = fopen(path_destino, "w");
@@ -98,28 +139,29 @@ void cifrador(int accion, char * path_origen, char * path_destino) {
 		system("exit");
 	}
 
-	fgets(encabezado, tamanio, archivo_origen);
-	fwrite(encabezado, tamanio, 1, archivo_destino);
-
-	long int tamanio_lectura = tamanio_archivo(path_origen) - tamanio;
-	long int tamanio_escritura;
-
 	if (accion == 0) {
 		tamanio_escritura = tamanio_lectura * 3;
 	} else {
-		tamanio_escritura = (long int) floor(tamanio_lectura/3);
+		tamanio_escritura = (long int) floor(tamanio_lectura / 3);
 	}
 
-	u8 * lector = malloc(tamanio_lectura);
-	u8 * output = malloc(tamanio_escritura);
+	fread(encabezado, tamanio, 1, archivo_origen);
+	char * impresion = malloc(tamanio_escritura + tamanio);
 
+	char * lector = malloc(tamanio_lectura);
+	char * output = malloc(tamanio_escritura);
 	fread(lector, tamanio_lectura, 1, archivo_origen);
-	//fgets(lector, tamanio_original, archivo_origen);
 
-	ECRYPT_process_packet(accion, ctx, &iv, lector, output, tamanio_lectura);
+	if (accion == 0) {
+		ECRYPT_process_packet(accion, ctx, &iv, lector, output, tamanio_lectura);
+	} else {
+		ECRYPT_process_packet(accion, ctx, &iv, lector, output, tamanio_escritura);
+	}
 
 	//creo el archivo
-	fwrite(output, tamanio_escritura, 1, archivo_destino);
+	memcpy(impresion, encabezado, tamanio);
+	memcpy(impresion + tamanio, output, tamanio_escritura);
+	fwrite(impresion, tamanio_escritura + tamanio, 1, archivo_destino);
 
 	free(output);
 	free(lector);
@@ -130,9 +172,9 @@ void cifrador(int accion, char * path_origen, char * path_destino) {
 }
 
 /*
-	while (feof(archivo_origen) == 0) {
-		fread(lector, tamanio_lectura, 1, archivo_origen);
-		ECRYPT_process_packet(accion, ctx, &iv, lector, output, tamanio_lectura);
-		fwrite(output, tamanio_escritura, 1, archivo_destino);
-	}
+ while (feof(archivo_origen) == 0) {
+ fread(lector, tamanio_lectura, 1, archivo_origen);
+ ECRYPT_process_packet(accion, ctx, &iv, lector, output, tamanio_lectura);
+ fwrite(output, tamanio_escritura, 1, archivo_destino);
+ }
  */
